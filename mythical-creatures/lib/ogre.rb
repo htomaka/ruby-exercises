f2512304a5ebcbb4b82693453e8782192a31c54f
tomaka <htomaka@treeptik.fr>
2018-12-15 18:44:43 +0100

    completed ogre

---
+++ mythical-creatures/lib/ogre.rb
@@ -0,0 +1,30 @@
+class Ogre
+    attr_reader :name,
+                :home,
+                :swings,
+                :encounter_counter
+
+    def initialize(name, home = "Swamp")
+        @name = name
+        @home = home
+        @swings = 0
+        @encounter_counter = 0
+    end
+
+    def encounter(human)
+        human.encounter_counter += 1
+        @encounter_counter += 1
+
+        if human.notices_ogre?
+            swing_at(human)
+        end
+    end
+
+    def swing_at(human)
+        @swings += 1
+    end
+
+    def apologize(human)
+        human.encounter_counter -= 1
+    end
+end
